3aa70f1c75d75a5673c2c4f7b65c792dea11d145
Stefan Seil <stefan.seil@mailbox.org>
2020-10-11 13:32:48 +0200

    Implement bounded degree network with non-uniform degree distribution

---
+++ src/seom/networks/BoundedDegree.java
@@ -0,0 +1,41 @@
+package seom.networks;
+
+import ec.util.MersenneTwisterFast;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+import seom.Agent;
+import seom.Main;
+import seom.Relationship;
+
+public class BoundedDegree extends UndirectedSparseGraph<Agent, Relationship> {
+    /* Note:
+     * This implementation creates a graph in which the different possible degrees
+     * (between minDegree and maxDegree) are not uniformly distributed among the nodes.
+     * While he doesn't mention it explicitly, it looks like Alexander uses such a kind
+     * of bounded degree network, too. Therefore, it seems best to keep this implementation,
+     * but add another one whereby the degrees are uniformly distributed.
+     */
+    public BoundedDegree(int numAgents, int minDegree, int maxDegree) {
+        MersenneTwisterFast random = new MersenneTwisterFast();
+        random.setSeed(Main.seed);
+
+        Agent[] agents = new Agent[numAgents];
+        for (int i = 0; i < numAgents; i++) {
+            agents[i] = new Agent(this);
+            addVertex(agents[i]);
+        }
+
+        for (Agent agent : agents) {
+            int degree = minDegree + random.nextInt(maxDegree - minDegree + 1);
+
+            for (int j = getNeighborCount(agent); j < degree; j++) {
+                Agent other;
+                do {
+                    int rand = random.nextInt(numAgents);
+                    other = agents[rand];
+                } while (other == agent || isNeighbor(other, agent) || getNeighborCount(other) == maxDegree);
+
+                addEdge(new Relationship(), agent, other);
+            }
+        }
+    }
+}
