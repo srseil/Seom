50c6c8c18c357b180cb762c030bbf14b6b00c058
Stefan Seil <stefan.seil@mailbox.org>
2020-10-11 13:33:06 +0200

    Add skeletons for small world and dynamic networks

---
+++ src/seom/networks/DynamicNetwork.java
@@ -0,0 +1,11 @@
+package seom.networks;
+
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import seom.Agent;
+import seom.Relationship;
+
+public class DynamicNetwork extends DirectedSparseGraph<Agent, Relationship> {
+    public DynamicNetwork(int numAgents) {
+
+    }
+}
---
+++ src/seom/networks/SmallWorld.java
@@ -0,0 +1,11 @@
+package seom.networks;
+
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+import seom.Agent;
+import seom.Relationship;
+
+public class SmallWorld extends UndirectedSparseGraph<Agent, Relationship> {
+    public SmallWorld(int numAgents) {
+
+    }
+}
