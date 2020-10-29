e239d5a1d6f5b85086bea0f664557aed1a1dd519
Stefan Seil <stefan.seil@mailbox.org>
2020-10-29 18:34:32 +0100

    Add radius parameter to 1D lattice

--- src/seom/networks/Lattice1D.java
+++ src/seom/networks/Lattice1D.java
@@ -5,18 +5,28 @@ import seom.Agent;
 import seom.Relationship;
 
 public class Lattice1D extends UndirectedSparseGraph<Agent, Relationship> {
-    public Lattice1D(int length, boolean wrapAround) {
+    public Lattice1D(int length, int radius, boolean wrapAround) {
+        assert length > 1 : "Length must be at least 2";
+        assert radius <= length / 2 : "Maximum radius is length / 2";
+
         Agent[] agents = new Agent[length];
         for (int i = 0; i < length; i++) {
             Agent agent = new Agent();
             agents[i] = agent;
             addVertex(agent);
+        }
 
-            if (i > 0) {
-                addEdge(new Relationship(), agents[i - 1], agent);
-            }
-            if (i == length - 1 && wrapAround) {
-                addEdge(new Relationship(), agent, agents[0]);
+        for (int i = 0; i < length; i++) {
+            int neighborIndex = i;
+            for (int j = 0; j < radius; j++) {
+                if (neighborIndex < length - 1) {
+                    neighborIndex += 1;
+                } else if (neighborIndex == length - 1 && wrapAround) {
+                    neighborIndex = 0;
+                } else {
+                    break;
+                }
+                addEdge(new Relationship(), agents[i], agents[neighborIndex]);
             }
         }
     }
