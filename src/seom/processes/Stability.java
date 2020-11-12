e317ae159b2403cd6c7686154712b0baeb3bf69b
Stefan Seil <stefan.seil@mailbox.org>
2020-11-12 12:22:21 +0100

    Implement cycle detection

--- src/seom/processes/Stability.java
+++ src/seom/processes/Stability.java
@@ -4,25 +4,68 @@ import seom.Agent;
 import seom.Simulation;
 import seom.games.Strategy;
 import sim.engine.SimState;
-import sim.engine.Steppable;
 
-public class Stability implements Steppable {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Stability implements SimulationProcess {
     private final Simulation simulation;
+    private final Map<Agent, Strategy> strategyMap;
+    private final List<Integer> strategyProgression;
+
+    private boolean cycleDetectionEnabled;
 
     public Stability(Simulation simulation) {
         this.simulation = simulation;
+        strategyMap = new HashMap<>();
+        strategyProgression = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isStochastic() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+        strategyProgression.clear();
     }
 
     @Override
     public void step(SimState simState) {
         System.out.println("Stability");
 
+        // Cycle detection
+        if (cycleDetectionEnabled) {
+            strategyMap.clear();
+            for (Agent agent : simulation.getConfig().getNetwork().getVertices()) {
+                strategyMap.put(agent, agent.getStrategy());
+            }
+
+            int currentHashCode = strategyMap.hashCode();
+            for (int hashCode : strategyProgression) {
+                if (hashCode == currentHashCode) {
+                    int start = strategyProgression.indexOf(hashCode) + 1;
+                    int end = strategyProgression.size() + 1;
+                    simulation.getResult().setCycle(start, end);
+                    simulation.kill();
+                    System.out.println("Cycle from " + start + " to " + end);
+                }
+            }
+
+            strategyProgression.add(currentHashCode);
+            System.out.println(strategyProgression.size() + " hash codes");
+        }
+
+        // Only one strategy left
         boolean stable = true;
-        Strategy stableStratey = null;
+        Strategy stableStrategy = null;
         for (Agent agent : simulation.getConfig().getNetwork().getVertices()) {
-            if (stableStratey == null) {
-                stableStratey = agent.getStrategy();
-            } else if (stableStratey != agent.getStrategy()) {
+            if (stableStrategy == null) {
+                stableStrategy = agent.getStrategy();
+            } else if (stableStrategy != agent.getStrategy()) {
                 stable = false;
                 break;
             }
@@ -31,5 +74,9 @@ public class Stability implements Steppable {
         if (stable) {
             simulation.kill();
         }
+    }
+
+    public void setCycleDetectionEnabled(boolean cycleDetectionEnabled) {
+        this.cycleDetectionEnabled = cycleDetectionEnabled;
     }
 }
