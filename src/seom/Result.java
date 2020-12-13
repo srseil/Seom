784fbfe61a0c8e4750ba277c57e35a4aaeff5580
Stefan Seil <stefan.seil@mailbox.org>
2020-12-13 14:12:46 +0100

    Adjust output computation in results

--- src/seom/Result.java
+++ src/seom/Result.java
@@ -2,10 +2,15 @@ package seom;
 
 import seom.games.Strategy;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Result {
     private final Configuration config;
+
+    private List<Strategy[]> strategyHistory;
+    private Map<Strategy, Integer> strategyMap;
 
     private long generationCount;
     private boolean cyclic;
@@ -20,6 +25,12 @@ public class Result {
     }
 
     public void computeOutputs(List<Strategy[]> strategyHistory) {
+        this.strategyHistory = strategyHistory;
+        computeStrategyDistribution();
+        computeMorality();
+    }
+
+    private void computeStrategyDistribution() {
         int start = 0;
         int end = strategyHistory.size() - 1;
         stability = 1.0 / config.getMaxNumGenerations();
@@ -30,22 +41,43 @@ public class Result {
             stability = 1.0 / getCycleLength();
         }
 
-        double relativeMorality = 0.0;
+        strategyMap = new HashMap<>();
         for (int i = start; i <= end; i++) {
-            Strategy[] strategies = strategyHistory.get(i);
-            int moralAgents = 0;
-            for (Strategy strategy : strategies) {
-                for (Strategy moralStrategy : config.getGame().getMoralStrategies()) {
-                    if (strategy == moralStrategy) {
-                        moralAgents++;
-                        break;
-                    }
+            for (Strategy strategy : strategyHistory.get(i)) {
+                int numAgents = strategyMap.getOrDefault(strategy, 0);
+                strategyMap.put(strategy, numAgents + 1);
+            }
+        }
+    }
+
+    private void computeMorality() {
+        int moralAgents = 0;
+        for (var entry : strategyMap.entrySet()) {
+            for (Strategy moralStrategy : config.getGame().getMoralStrategies()) {
+                if (entry.getKey() == moralStrategy) {
+                    moralAgents += entry.getValue();
+                    break;
                 }
             }
-            relativeMorality += (double) moralAgents / strategies.length;
         }
 
-        morality = relativeMorality / getCycleLength();
+        int numAgents = strategyHistory.get(0).length;
+        int numGenerations = cyclic ? getCycleLength() : strategyHistory.size();
+        morality = (double) moralAgents / (numAgents * numGenerations);
+    }
+
+    @Override
+    public String toString() {
+        if (!cyclic) {
+            return "(no cycle, " + "stability: " + stability + ", morality: " + morality + ")";
+        } else {
+            return "(" + "cycle length: " + getCycleLength() + ", "
+                + "stability: " + stability + ", morality: " + morality + ")";
+        }
+    }
+
+    public Map<Strategy, Integer> getStrategyMap() {
+        return strategyMap;
     }
 
     public long getGenerationCount() {
