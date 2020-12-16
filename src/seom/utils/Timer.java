428e9ba167d1286c3fad5fddee0adfc71e5d432e
Stefan Seil <stefan.seil@mailbox.org>
2020-12-16 16:08:22 +0100

    Enhance static timing facility with stack-based recording

--- src/seom/utils/Timer.java
+++ src/seom/utils/Timer.java
@@ -1,16 +1,18 @@
 package seom.utils;
 
 import java.time.Duration;
+import java.util.Stack;
 
 public class Timer {
-    private static long start = 0;
+    private static final Stack<Long> startStack = new Stack<>();
     private static Duration duration = Duration.ZERO;
 
     public static void start() {
-        start = System.nanoTime();
+        startStack.push(System.nanoTime());
     }
 
     public static Duration stop() {
+        long start = startStack.pop();
         long stop = System.nanoTime();
         duration = Duration.ofNanos(stop - start);
         return duration;
