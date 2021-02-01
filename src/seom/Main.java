a94ffce3c439ded1681396eda16e44084131b280
Stefan Seil <stefan.seil@mailbox.org>
2021-02-01 21:34:38 +0100

    Simplify main method and add special case for fully connected networks

--- src/seom/Main.java
+++ src/seom/Main.java
@@ -7,19 +7,19 @@ import seom.utils.Log;
 
 public class Main {
     public static void main(String[] args) {
-        var gameTypes = new GameType[] {
-            GameType.PrisonersDilemma,
-            GameType.StagHunt,
-            GameType.BargainingSubgame,
-            GameType.UltimatumGame
-        };
-
-        for (GameType gameType : gameTypes) {
+        for (GameType gameType : GameType.values()) {
             for (NetworkType networkType : NetworkType.values()) {
                 String logPath = "analysis/" + gameType + "/" + networkType + "/log.txt";
                 Log.reset();
                 Log.enableFileLogging(logPath);
                 Log.enableSimpleLogging();
+
+                // Use fewer repetitions for fully connected networks due to bad performance
+                if (networkType == NetworkType.FullyConnected) {
+                    SensitivityAnalysis.NUM_REPETITIONS = 10;
+                } else {
+                    SensitivityAnalysis.NUM_REPETITIONS = 100;
+                }
 
                 var analysis = new SensitivityAnalysis(gameType, networkType);
                 analysis.simulate();
